11b3efd8fc7de3a5093230ecf52c574c5594a9f1
Kheldin <kheldin.acherchour@gmail.com>
2026-06-03 14:39:58 +0200

    refactor: removed unused sprite

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from game.game_loop import game_loop
 def main():
     try:
         map = parse_file()
-
+        print(map.start_hub)
         pygame.init()
         game_loop(map)
     except Exception as e:
